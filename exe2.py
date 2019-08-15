441262879d0d431d6b3fe68395d93dbb22bad45c
ElcimarSilva <elcimar8@gmail.com>
2019-08-15 19:42:54 -0300

    'Alteração'

--- exe2.py
+++ exe2.py
@@ -1,15 +1,28 @@
 #2 - Escreva um algoritmo que solicita ao usuário para digitar um número inteiro positivo,
 #e mostre-o por extenso. Este número deverá variar entre 1 e 5. Se o usuário introduzir
 #um número que não pertença a este intervalo, mostre a frase “número inválido”.
-NumMaior = 5
-NumMenor = 0
-Numero = int (input("digite um numero inteiro positivo"))
-def Calculo()
-    if Numero <= NumMaior:
-        print (Numero) # Por extenso
-    elif
-        Numero >= NumMenor
-        print(Numero)  # Por extenso
-    else
-        print "Numero Invalido"
+class MediaAluno():
+    def maiorMedia(self):
+        nota1 = int(input("Nota 1: "))
+        nota2 = int(input("Nota 2: "))
+        nota3 = int(input("Nota 3: "))
+        media = ((nota1 + nota2 + nota3) / 3)  # Calculo de media
 
+        # Verifica se maior que 18 "Aprovado"
+        if media >= 18:
+            print("Aprovado")
+        else:
+            print("Menor que 18")
+
+chama=MediaAluno()
+
+while True:
+    opcao=int(input('1-Para começar a inserir suas notas \n'
+                    '0-Sair'
+                    ))
+    if opcao ==1:
+        chama.maiorMedia()
+    elif opcao ==0:
+        exit()
+    else:
+        print("Opção Invalida")
--- exe6.py
+++ exe6.py
@@ -1,1 +1,6 @@
-print ("Olá")
+#6 - Faça um algoritmo que leia um nº inteiro e mostre uma
+#mensagem indicando se este número é par ou ímpar, e se é positivo ou negativo
+
+numero=int(input("Digite o numero a ser verificado"))
+num_par=numero%2
+if numero
