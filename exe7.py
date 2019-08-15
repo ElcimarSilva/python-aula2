875ad69894d10103780bca8b776af22dc3f87746
ElcimarSilva <elcimar8@gmail.com>
2019-08-15 20:04:18 -0300

    'new'

---
+++ exe7.py
@@ -0,0 +1,4 @@
+#7 - Escreva um algoritmo que leia 10 números informados pelo usuário e, depois,
+#informe o menor, número, o maior número, a soma dos números informados e a média
+#aritmética dos números informados.
+
