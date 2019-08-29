f13b065658c035a4a8a727bbe7a61e0f3e061856
ElcimarSilva <elcimar8@gmail.com>
2019-08-29 19:30:39 -0300

    'exe14exe15'

--- exe14.py
+++ exe14.py
@@ -2,3 +2,4 @@
 
 for i in range(10):
     print(f"8 x {i} = {8*i}")# Cada vez que realiza p for acrescenta 1 ao (i)
+
--- exe15.py
+++ exe15.py
@@ -1,3 +1,13 @@
 #15 - Ler 10 valores (considere que não serão lidos valores iguais) e escrever o maior deles.
 lista = []
+menor10 = 0
+while menor10 <10:
+    valor = int(input("Digite um valor: "))
+    if valor in lista:
+        print("Numero repetido!!")
+    else:
+        menor10=menor10+1
+        lista.append(valor)
+maior = max(lista)
+print (maior) # Falta Mostrar bonitinho
 
